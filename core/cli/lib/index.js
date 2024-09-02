8d2480ac91381e384579c506ca3ca7338ec6f374
chenying <fun20001016@163.com>
2024-09-02 23:58:59 +0800

    feat: 检查node版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,11 +2,33 @@
 
 module.exports = core;
 
-const pkg = require("../package.json");
+const semver = require("semver");
+const colors = require("colors");
 const log = require("@cycli/log");
+const pkg = require("../package.json");
+const constant = require("./const");
 
 function core() {
-  checkPkgVersion();
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (e) {
+    log.error(e.message);
+  }
+}
+
+/**
+ * 检查node版本号
+ */
+function checkNodeVersion() {
+  const currentVersion = process.version;
+  const lowestNodeVersion = constant.LOWEST_NODE_VERSION;
+
+  if (!semver.gte(currentVersion, lowestNodeVersion)) {
+    throw new Error(
+      colors.red(`cycli 需要安装 v${lowestNodeVersion} 以上版本的Node.js`)
+    );
+  }
 }
 
 /**
